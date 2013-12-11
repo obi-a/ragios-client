6e6cebdf711d584a313a5871b960af8be651ded4
obi-a <obioraakubue@yahoo.com>
2013-12-10 23:40:38 -0500

    TODO

--- ragios_client_spec.rb
+++ ragios_client_spec.rb
@@ -1,6 +1,14 @@
 #specs for ragios-client
 require 'ragios-client'
 
+
+#communicating with server ->
+#if no token get a token || current token is invalid
+#get a new token
+#apply this to couchdb admin on ragios, so that username and password is not sent  to server
+#to get a token for every request
+#improvement in terms of security, will deal with performance improvements when necessary
+
 ragios = Ragios::Client.new
 
 monitors = [{monitor: "my website",
