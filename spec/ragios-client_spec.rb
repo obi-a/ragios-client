3561a34fb8e178864b86ab57ac2ccc4823a36081
The Rainmaker <obioraakubue@yahoo.com>
2014-08-21 22:24:30 -0400

    cleanup the test suite to be more maintainable

--- spec/ragios-client_spec.rb
+++ spec/ragios-client_spec.rb
@@ -147,63 +147,39 @@ describe "Ragios Client" do
         #stop monitor is idempotent
         @ragios.stop(@monitor_id).should == {ok: true}
       end
+      it "cannot stop a monitor that dont exist" do
+        monitor_id = "dont_exist"
+        expect { @ragios.stop(monitor_id) }.to raise_error(Ragios::ClientException, "No monitor found with id = #{monitor_id}")
+      end
+    end
+    describe "#restart" do
+      it "restarts a stopped monitor" do
+        @ragios.stop(@monitor_id)
+        stopped_monitor = @ragios.find(@monitor_id)
+        stopped_monitor[:status_].should == "stopped"
+
+        @ragios.restart(@monitor_id).should == {ok: true}
+
+        active_monitor = @ragios.find(@monitor_id)
+        active_monitor[:status_].should == "active"
+
+        #restart monitor is idempotent
+        @ragios.restart(@monitor_id).should == {ok: true}
+      end
+      it "cannot restart a monitor that dont exist" do
+        monitor_id = "dont_exist"
+        expect { @ragios.restart(monitor_id) }.to raise_error(Ragios::ClientException, "No monitor found with id = #{monitor_id}")
+      end
+    end
+    describe "#all" do
+      it "retrieves all monitors" do
+        retrieved_monitors = @ragios.all
+        retrieved_monitors.should_not be_empty
+        retrieved_monitors.should be_an_instance_of Array
+      end
     end
     after(:each) do
       @ragios.delete(@monitor_id)
-    end
-  end
-
-
-  it "cannot stop a monitor that dont exist" do
-    monitor_id = "dont_exist"
-    begin
-      @ragios.stop(monitor_id)
-    rescue => e
-      e.should be_an_instance_of Ragios::ClientException
-      e.message.should include("No monitor found with id = #{monitor_id}")
-    end
-  end
-
-  it "restarts a stopped monitor" do
-    #setup starts
-    unique_name = "Google #{Time.now.to_i}"
-    monitors = [{monitor: unique_name,
-      url: "http://google.com",
-      every: "5m",
-      contact: "admin@mail.com",
-      via: ["gmail_notifier"],
-      plugin: plugin }]
-
-    returned_monitors = @ragios.add monitors
-    monitor_id = returned_monitors.first[:_id]
-    #setup ends
-
-    @ragios.stop(monitor_id)
-    stopped_monitor = @ragios.find(monitor_id)
-    stopped_monitor[:status_].should == "stopped"
-
-    hash = @ragios.restart(monitor_id)
-    hash[:ok].should == true
-
-    active_monitor = @ragios.find(monitor_id)
-    active_monitor[:status_].should == "active"
-
-    #restart monitor is idempotent
-    hash = @ragios.restart(monitor_id)
-    hash[:ok].should == true
-
-    #teardown
-    hash = @ragios.delete(monitor_id)
-    hash[:ok].should == true
-  end
-
-  it "cannot restart a monitor that dont exist" do
-    monitor_id = "dont_exist"
-    begin
-      @ragios.restart(monitor_id)
-    rescue => e
-      e.should be_an_instance_of Ragios::ClientException
-      e.message.should include("No monitor found with id = #{monitor_id}")
     end
   end
 
@@ -212,51 +188,23 @@ describe "Ragios Client" do
   end
 
   it "rejects incorrect login credentials" do
-    begin
-      @ragios.login('incorret','incorret')
-    rescue => e
-      e.should be_an_instance_of Ragios::ClientException
-      e.message.should include("You are not authorized to access this resource")
-    end
-  end
-
-  it "retrieves all monitors" do
-    #setup starts
-    unique_name = "Google #{Time.now.to_i}"
-    monitors = [{monitor: unique_name,
-      url: "http://google.com",
-      every: "5m",
-      contact: "admin@mail.com",
-      via: ["gmail_notifier"],
-      plugin: plugin }]
-
-    returned_monitors = @ragios.add monitors
-    monitor_id = returned_monitors.first[:_id]
-    #setup ends
-
-    retrieved_monitors = @ragios.all
-    retrieved_monitors.should_not be_empty
-    retrieved_monitors.should be_an_instance_of Array
-
-    #teardown
-    hash = @ragios.delete(monitor_id)
-    hash[:ok].should == true
+    expect { @ragios.login('incorret','incorret') }.to raise_error(Ragios::ClientException, "You are not authorized to access this resource")
   end
 
   it "has default settings" do
     ragios_client = Ragios::Client.new
     ragios_client.address.should == "http://127.0.0.1"
     ragios_client.port.should == "5041"
-    ragios_client.username.should == ""
-    ragios_client.password.should == ""
+    ragios_client.username.should == nil
+    ragios_client.password.should == nil
   end
 
   it "accepts new default address" do
     ragios_client = Ragios::Client.new(address: 'https://mysite.com', port: '6802')
     ragios_client.address.should == "https://mysite.com"
     ragios_client.port.should == "6802"
-    ragios_client.username.should == ""
-    ragios_client.password.should == ""
+    ragios_client.username.should == nil
+    ragios_client.password.should == nil
   end
 
   it "accepts new default login credentials" do
@@ -267,12 +215,11 @@ describe "Ragios Client" do
     ragios_client.password.should == "abc123"
   end
 
-    it "accepts new default settings" do
+  it "accepts new default settings" do
     ragios_client = Ragios::Client.new(username: 'superadmin', password: 'abc123', address: 'https://mysite.com', port: '6802')
     ragios_client.address.should == "https://mysite.com"
     ragios_client.port.should == "6802"
     ragios_client.username.should == "superadmin"
     ragios_client.password.should == "abc123"
   end
-
 end
